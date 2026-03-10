bee068595c04a9482866bb713e01989ade4c979b
USMAN TARIQ <utariq769@gmail.com>
2026-03-11 03:29:17 +0500

    changed url

---
+++ app/app-development/page.jsx
@@ -0,0 +1,46 @@
+import DetailFormSection from "../components/detail-form-section";
+import Footer from "../components/footer";
+import Header from "../components/header";
+import IndustriesMobile from "../components/industries-mobile";
+import LpAppAbout from "../components/lp-app-about";
+import LpAppBarriers from "../components/lp-app-barriers";
+import LpAppCase from "../components/lp-app-case";
+import LpAppHire from "../components/lp-app-hire";
+import LpAppPartnership from "../components/lp-app-partnership";
+import LpAppPlatform from "../components/lp-app-platform";
+import LpAppprocess from "../components/lp-app-process";
+import LpAppServices from "../components/lp-app-services";
+import LpAppTechStack from "../components/lp-app-tech-stack";
+import LpAppWhyCustom from "../components/lp-app-why-custom";
+import TestimonialSection from "../components/testimonial-section";
+import WebAppHeroSection from "../components/web-app-hero-section";
+
+const AppDevelopmentPage = () => {
+  return (
+    <div>
+      <Header />
+      <WebAppHeroSection autoHeight />
+      <LpAppAbout />
+      <LpAppServices />
+      <IndustriesMobile
+        secondWord="Impact of Mobile Application Development Across Industries"
+        secondCol="!col-span-4 max-sm:!col-span-12"
+        thirdCol="!col-span-8 max-sm:!col-span-12"
+        para="From enhancing customer engagement to modernizing legacy systems, industry-specific mobile solutions are enabling organizations to adapt and thrive in an increasingly digital world."
+      />
+      <LpAppprocess/>
+      <LpAppBarriers/>
+      <LpAppWhyCustom />
+      <LpAppPartnership />
+      <LpAppTechStack />
+      <LpAppHire />
+      <LpAppPlatform />
+      <TestimonialSection />
+      <LpAppCase />
+      <DetailFormSection />
+      <Footer />
+    </div>
+  );
+};
+
+export default AppDevelopmentPage;

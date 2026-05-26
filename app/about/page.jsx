abe9f7e43c4fd731fb733e51722b7cf49857e1d2
USMAN TARIQ <utariq769@gmail.com>
2026-05-27 02:46:56 +0500

    fixed ui

--- app/about/page.jsx
+++ app/about/page.jsx
@@ -12,7 +12,7 @@ import Footer from "../components/footer";
 import WhoWeAre from "../components/who-we-are";
 
 export const metadata = {
-  alternates: { canonical: "/about" },
+  alternates: { canonical: "/about" }, 
 };
 
 const About = () => {
